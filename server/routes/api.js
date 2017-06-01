28da9d166ebc8b03b4cffb27dfa36eee7ef3a158
Shaowen Ren <jackren1991@gmail.com>
2017-05-31 22:07:47 -0700

    Implemented voice convert/recognize on test file, create new endpoint controller for audio comments, need to integrate next

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const voiceRecognize = require('../service/voiceRecognize.js');
 
 router.route('/')
   .get((req, res) => {
@@ -11,4 +12,5 @@ router.route('/')
     res.status(201).send({ data: 'Posted!' });
   });
 
+voiceRecognize.asyncRecognize('jack_1.aac', 'FLAC', 22050, 'en-US');
 module.exports = router;
