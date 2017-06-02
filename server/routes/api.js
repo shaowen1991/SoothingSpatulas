8887c93b5b496e37af7cf17741bccb669a9a2efe
Shaowen Ren <jackren1991@gmail.com>
2017-06-01 18:31:22 -0700

    Got audio file upload to server, but without sample rate (server)

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const voiceRecognize = require('../service/voiceRecognize.js');
 
 router.route('/')
   .get((req, res) => {
