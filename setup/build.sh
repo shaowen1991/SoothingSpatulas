3cc1fe7a3f702084b58f469fc40bf12de15adfe5
Nick Anderson <nicasioca@gmail.com>
2017-06-03 15:27:51 -0700

    update docker-build command for yarn

--- setup/build.sh
+++ setup/build.sh
@@ -2,7 +2,7 @@
 
 # Make sure you have permissions to execute the file
 # chmod +x ./setup/build.sh
-docker stop momento 2>/dev/null
-docker rm momento 2>/dev/null
-docker rmi momento-image 2>/dev/null
+docker stop $(docker ps -a -q) 2>/dev/null
+docker rm $(docker ps -a -q) 2>/dev/null
+docker rmi $(docker images -q) 2>/dev/null
 docker build -t momento-image . 2>/dev/null
